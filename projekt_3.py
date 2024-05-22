d502b257fbfa3a3079ffda52c57f6b76586bccde
Skala92 <160157178+Skala92@users.noreply.github.com>
2024-05-22 14:51:08 +0200

    Add files via upload

--- projekt_3.py
+++ projekt_3.py
@@ -13,55 +13,61 @@ from bs4 import BeautifulSoup
 URL = "https://volby.cz/pls/ps2017nss/"
 
 def main(url, output_file):
-    with open(output_file, mode="w", encoding="utf-8-sig", newline='') as file:
-        writer = csv.writer(file, delimiter=";")
+    try:
+        print("Starting data processing...")
+        with open(output_file, mode="w", encoding="utf-8-sig", newline='') as f:
+            writer = csv.writer(f, delimiter=";")
 
-        header = False
-        scrap = requests.get(url)
-        soup = BeautifulSoup(scrap.text, "html.parser")
-        regions = soup.find_all("td", {"class": "cislo"})
+            header = False
+            scrap = requests.get(url)
+            soup = BeautifulSoup(scrap.text, "html.parser")
+            regions = soup.find_all("td", {"class": "cislo"})
 
-        for line in regions:
-            region = []
-            region = get_code_name(line, region)
-            region_soup = get_soup(URL, line)
-            region_results = region_soup.find(id="ps311_t1")
-            region = get_voters(region_results, region)
-            parties = region_soup.find(id="inner").find_all("tr")
-            region = get_party_votes(parties, region)
+            for line in regions:
+                region_data = []
+                region_data = get_id_name(line, region_data)
+                region_soup = get_soup(URL, line)
 
-            if not header:
-                column_names = ["Code", "Location", "Registered", "Envelopes", "Valid"]
-                for new_line in parties:
-                    if not new_line.find("th"):
-                        column_names.append(new_line.find_all("td")[1].string)
-                writer.writerow(column_names)
-                header = True
+                region_results = region_soup.find(id="ps311_t1")
+                region_data = get_voters(region_results, region_data)
 
-            writer.writerow(region)
+                parties = region_soup.find(id="inner").find_all("tr")
+                region_data = get_party_votes(parties, region_data)
 
-    print(f"File {output_file} is ready.")
+                if not header:
+                    column_names = ["Code", "Location", "Registered", "Envelopes", "Valid"]
+                    for new_line in parties:
+                        if not new_line.find("th"):
+                            column_names.append(new_line.find_all("td")[1].string)
+                    writer.writerow(column_names)
+                    header = True
+
+                writer.writerow(region_data)
+
+        print(f"Data processing finished. File {output_file} is ready.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def get_id_name(line, lst):
+    lst.append(line.find("a").string)
+    lst.append(line.parent.find_all()[2].string)
+    return lst
 
 def get_soup(URL, line):
     region_url = requests.get(URL + line.find("a").attrs["href"])
     return BeautifulSoup(region_url.text, "html.parser")
 
-def get_code_name(line, list):
-    list.append(line.find("a").string)
-    list.append(line.parent.find_all()[2].string)
-    return list
+def get_voters(region_results, lst):
+    lst.append(region_results.find("td", {"class": "cislo", "headers": "sa2"}).string)
+    lst.append(region_results.find("td", {"class": "cislo", "headers": "sa3"}).string)
+    lst.append(region_results.find("td", {"class": "cislo", "headers": "sa6"}).string)
+    return lst
 
-def get_voters(region_results, list):
-    list.append(region_results.find("td", {"class": "cislo", "headers": "sa2"}).string)
-    list.append(region_results.find("td", {"class": "cislo", "headers": "sa3"}).string)
-    list.append(region_results.find("td", {"class": "cislo", "headers": "sa6"}).string)
-    return list
-
-def get_party_votes(parties, list):
+def get_party_votes(parties, lst):
     for line in parties:
         if not line.find("th"):
-            list.append(line.find_all("td", {"class": "cislo"})[1].string)
-    return list
+            lst.append(line.find_all("td", {"class": "cislo"})[1].string)
+    return lst
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Elections Scraper')
